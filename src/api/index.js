1cfc375bff97bcadd6ef031ddfeaa966658a04c7
San-53 <86125077+San-53@users.noreply.github.com>
2022-03-31 20:37:22 +0530

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+
+
+
+
+import axios from 'axios';
+
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+
+export const getPlacesData = async (type,sw, ne) => {
+  try {
+    //const { data: { data } } = await axios.get(URL, options);
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        // 'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        // 'x-rapidapi-key': 'a691bebc4cmsh8eee47137fedc16p1772a3jsnc3c07a84cd85'
+        // 'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        // 'x-rapidapi-key': '6b22b78a31msh13f89920d74dd79p13cdefjsn8435e6fcc907'
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd47fd7229amsh655453f37017303p12452ejsnc4504b69f182'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getWeatherData = async (lat,lng) => {
+  try {
+    const {data} = await axios.get('https://community-open-weather-map.p.rapidapi.com/find',{
+      params: {lon: lng,lat: lat, },
+      headers: {
+        // 'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        // 'x-rapidapi-key': '6b22b78a31msh13f89920d74dd79p13cdefjsn8435e6fcc907'
+        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd47fd7229amsh655453f37017303p12452ejsnc4504b69f182'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ src/components/PlaceDetails/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  chip: {
+    margin: '5px 5px 5px 0',
+  },
+  subtitle: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
+  },
+  spacing: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+  },
+  card:{
+    // background: "#eb9d65"
+    // background: "#fffcd4"
+    background:"#ffffd9"
+    // #fffcd4
+  }
+}));
